70f9e31209685dd18bc6d95dac7d3c62ca96e752
Noe Cortez <corteznoe3@gmail.com>
2025-06-25 23:11:10 -0600

    feat: Inicializa del proyecto frontend con Vue 3, Vite y TypeScript
    - Se añadió el archivo package.json con dependencias para Vue, Pinia y Vue Router.
    - Se creó el punto de entrada principal de la aplicación (main.ts) y se configuró la aplicación Vue.
    - Se configuró Vue Router con una ruta de inicio básica.
    - Se añadió App.vue como componente principal con RouterView.
    - Se creó HomeView.vue para la página de inicio.
    - Se incluyeron recursos SVG para los logotipos de Vite y Vue.
    - Se establecieron los archivos de configuración de TypeScript para la aplicación y el nodo.
    - Se configuró Vite con el plugin de Vue.

---
+++ frontend/src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <RouterView />
+
+    <p>© 2023 - {{ new Date().getFullYear() }} My Application</p>
+  </div>
+</template>
+
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>Bienvenido a TaskForge</h1>
+</template>
